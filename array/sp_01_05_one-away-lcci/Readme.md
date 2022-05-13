732c44c3f0302527ecc867cf9157b82fb3702d20
赵晖 <>
2022-05-13 09:22:44 +0800

    增加题目

---
+++ array/sp_01_05_one-away-lcci/Readme.md
@@ -0,0 +1,33 @@
+
+来源：力扣（LeetCode）  
+链接：https://leetcode.cn/problems/one-away-lcci/
+
+字符串有三种编辑操作:插入一个字符、删除一个字符或者替换一个字符。 给定两个字符串，编写一个函数判定它们是否只需要一次(或者零次)编辑。
+
+
+
+示例：
+```
+输入: 
+first = "pale"
+second = "ple"
+输出: True
+```
+示例：
+```
+输入: 
+first = "pales"
+second = "pal"
+输出: False
+
+```
+
+
+
+提示：
+
+
+
+总结：
+* 确保参数长度关系的技巧
+* 判断条件的精简
---
+++ array/sp_01_05_one-away-lcci/cmd/answer.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(oneEditAway("ab", "bc"))
+}
+
+func oneEditAway(first, second string) bool {
+	m, n := len(first), len(second)
+	if m < n {
+		return oneEditAway(second, first)
+	}
+	if m-n > 1 {
+		return false
+	}
+	for i, ch := range second {
+		if first[i] != byte(ch) {
+			if m == n {
+				return first[i+1:] == second[i+1:]
+			}
+			return first[i+1:] == second[i:]
+		}
+	}
+	return true
+}

b76b6051a86055f0a768a83cfb1aa0a27b3a3c74
PJLAB\zhaohui <hui.zhao1@qq.com>
2022-07-08 09:45:51 +0800

    add description

--- trie/00440_k-th-smallest-in-lexicographical-order/cmd/answer.go
+++ trie/00440_k-th-smallest-in-lexicographical-order/cmd/answer.go
@@ -2,34 +2,39 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println(findKthNumber(122,16))
+func main() {
+	fmt.Println(findKthNumber(9, 4))
+	fmt.Println(findKthNumber(122, 16))
 }
 
-func count(pre ,end int)int{
-	ans:=0
-	s,e:=pre,pre
-	for s <= end{
-		ans += min(e,end) - s + 1
-		s *=10
-		e = e * 10 + 9
+// 从 1 ~ end 以 pre 开头的数字有多少个（含 pre）
+func count(pre, end int) int {
+	ans := 0
+	s, e := pre, pre
+	for s <= end {
+		ans += min(e, end) - s + 1
+		s *= 10
+		e = e*10 + 9
 	}
 	return ans
 }
 
 func findKthNumber(n, k int) int {
-	cur:=1
-	k-- // 根结点
-	for k > 0{
-		c:= count(cur,n)
-		if c > k{
-			// 前缀不变，继续寻找
+	// need 表示还差几个数
+	need := k
+	cur := 1
+	for need > 1 { // 还差1个的时候，就找到了要找的数字
+		// 当前前缀有几个数字
+		c := count(cur, n)
+
+		if c >= need {
+			// 当前前缀数字足够的话，就继续找
 			cur *= 10
-			k--
-		}else{
-			// 前缀+1
+			need--
+		} else {
+			// 当前前缀不够，跳过所有数字，去找下一个前缀
 			cur++
-			k-=c
+			need -= c
 		}
 	}
 	return cur
